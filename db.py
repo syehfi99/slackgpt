680583cf7130c329a038db437356e59dc2b3f15a
Ubuntu <ubuntu@ip-172-31-13-250.ap-southeast-1.compute.internal>
2023-06-03 14:04:10 +0000

    update

--- db.py
+++ db.py
@@ -7,30 +7,56 @@ firebase_admin.initialize_app(cred)
 
 firestore_db = firestore.client()
 
+
 def save_user_reply(message, user_id):
-    message_history = [{"role": "system", "content": "You are a intelligent assistant."}]
+    message_history = [
+        {"role": "system", "content": "You are a intelligent assistant."}
+    ]
     new_message = [*message_history, message]
-    firestore_db.collection(u'history_chat').document(f"chat_{user_id}").set({"chat": firestore.ArrayUnion(new_message)}, merge=True )
+    firestore_db.collection("history_chat").document(f"chat_{user_id}").set(
+        {"chat": firestore.ArrayUnion(new_message)}, merge=True
+    )
+
 
 def delete_user_reply(user_id):
-    firestore_db.collection(u'history_chat').document(f"chat_{user_id}").delete()
+    firestore_db.collection("history_chat").document(f"chat_{user_id}").delete()
+
 
 def get_user_reply(user_id):
-    doc_ref = firestore_db.collection('history_chat')
+    doc_ref = firestore_db.collection("history_chat")
     doc_chat = doc_ref.document(f"chat_{user_id}")
     doc = doc_chat.get().to_dict()
     return doc
-   
+
 
 def readDB(prompt):
-  doc_ref = firestore_db.collection('prompts')
-  prompt_bank = doc_ref.document('prompt_bank')
-  coll_prompt = prompt_bank.collection(f"{prompt}")
-  data_prompt = coll_prompt.document(f"prompt_{prompt}")
-  doc = data_prompt.get().to_dict()
-  return doc
-  # snapshots = firestore_db.collection('prompts').document('prompt_bank').collections()
-  # for collection in snapshots:
-  #   for doc in collection.stream():
-  #       print(f'{doc.id} => {doc.to_dict()}')
+    doc_ref = firestore_db.collection("prompts")
+    prompt_bank = doc_ref.document("prompt_bank")
+    coll_prompt = prompt_bank.collection(f"{prompt}")
+    data_prompt = coll_prompt.document(f"prompt_{prompt}")
+    doc = data_prompt.get().to_dict()
+    return doc
+    # snapshots = firestore_db.collection('prompts').document('prompt_bank').collections()
+    # for collection in snapshots:
+    #   for doc in collection.stream():
+    #       print(f'{doc.id} => {doc.to_dict()}')
     # return snapshot.to_dict()
+
+
+def readBankCollection():
+    data_array = []  # Array untuk menyimpan data
+    doc_ref = firestore_db.collection("prompts").stream()
+    for doc in doc_ref:
+        data_array.append({doc.id: doc.to_dict()})
+    return data_array
+    # doc_ref = firestore_db.collection("prompts")
+    # prompt_bank = doc_ref.document("prompt_bank")
+    # prompt_banks = prompt_bank.collections()
+
+    # data_array = []  # Array untuk menyimpan data
+
+    # for collection in prompt_banks:
+    #     for doc in collection.stream():
+    #         data_array.append({doc.id: doc.to_dict()})
+
+    # return data_array
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from slack_bolt import App
-from db import readDB, delete_user_reply
+from db import readDB, readBankCollection, delete_user_reply
 from chatgpt import chatGPT
 import pandas as pd
 import re
@@ -8,7 +8,11 @@ import fitz
 import os
 from dotenv import load_dotenv
 from slack_bolt.adapter.flask import SlackRequestHandler
-import chardet
+from llama_index import SimpleDirectoryReader
+from llama_index import GPTVectorStoreIndex
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders.csv_loader import CSVLoader
 
 load_dotenv()
 
@@ -16,29 +20,51 @@ app = App()
 
 bot_token = os.environ["SLACK_BOT_TOKEN"]
 
-blocks = [
-    {
-        "type": "section",
-        "text": {"type": "mrkdwn", "text": "Pilih prompt yang akan digunakan"},
-    },
-    {
-        "type": "actions",
-        "elements": [
+
+def startingBlocks():
+    startingData = readBankCollection()
+    data_array_bloks = []
+    for data in startingData:
+        key = list(data.keys())[0]
+        options = []
+        for option_value in data[key]['prompts']:
+            option = {
+                'text': {
+                    'type': 'plain_text',
+                    'text': option_value,
+                    'emoji': True,
+                },
+                'value': option_value,
+            }
+            options.append(option)
+
+        data_array_bloks.append(
             {
-                "type": "button",
-                "text": {"type": "plain_text", "text": "SEO"},
-                "value": "seo",
-                "action_id": "seo",
+                "type": "section",
+                "text": {"type": "mrkdwn", "text": f"{key}"},
+                "accessory": {
+                    "type": "radio_buttons",
+                    "options": options,
+                    "action_id": "radio_pilih_prompt",
+                },
+            }
+        )
+
+    blocks = [
+        *data_array_bloks,
+        {"type": "divider"},
+        {
+            "dispatch_action": True,
+            "type": "input",
+            "element": {
+                "type": "plain_text_input",
+                "multiline": True,
+                "action_id": "keyword_input",
             },
-            {
-                "type": "button",
-                "text": {"type": "plain_text", "text": "Finance"},
-                "value": "finance",
-                "action_id": "finance",
-            },
-        ],
-    },
-]
+        "label": {"type": "plain_text", "text": "Masukkan keyword anda", "emoji": True},
+        },
+    ]
+    return blocks
 
 input_blocks = [
     {
@@ -74,15 +100,15 @@ store_prompt = ""
 # Add functionality here
 @app.command("/devgpt-prompt")
 def prompt_chat(ack, say, command):
+    blocks = startingBlocks()
     ack()
-    say(blocks=blocks, text="Pilih prompt yang digunakan")
+    say(blocks=blocks)
 
 
 @app.command("/devgpt-write")
 def chatWithGPT(ack, say, command):
     ack()
     say(blocks=input_blocks, text="")
-
 
 @app.action("seo")
 def prompt_seo(ack, respond, body):
@@ -100,21 +126,43 @@ def prompt_finance(ack, respond, body):
     respond(blocks=input_blocks)
 
 
+@app.action("radio_pilih_prompt")
+def handle_some_action(ack, body, logger):
+    ack()
+    print('radio_pilih_prompt', body, flush=True)
+    payload = body
+    data = payload['state']['values'] if payload and 'state' in payload and 'values' in payload['state'] else None
+    radio_pilih_prompt = None
+    if data:
+        for key in data:
+            print('key', data, flush=True)
+            if 'radio_pilih_prompt' in data[key]:
+                radio_pilih_prompt = data[key]
+                break
+
+    print('result radio_pilih_prompt', radio_pilih_prompt['radio_pilih_prompt']['selected_option']['text']['text'], flush=True)
+
+
 @app.action("keyword_input")
 def keyword_input(ack, body, say):
     ack()
-    print(body)
-    user_id = body["user"]['id']
-    if store_prompt != "":
-        prompt = readDB(store_prompt)
-        print("ini prompt", prompt)
-        reply = chatGPT(prompt["prompt"] + " " + body["actions"][0]["value"])
-        say(f"{reply}")
-        say(blocks=input_blocks, text="")
-
-    reply = chatGPT(body["actions"][0]["value"], user_id)
+    print("keyword_input", body, flush=True)
+    data = body['state']['values'] if body and 'state' in body and 'values' in body['state'] else None
+    radio_pilih_prompt = None
+    if data:
+        for key in data:
+            print('key', data, flush=True)
+            if 'radio_pilih_prompt' in data[key]:
+                radio_pilih_prompt = data[key]['radio_pilih_prompt']['selected_option']['text']['text']
+                break
+    user_input = body["actions"][0]["value"]
+    user_id = body["user"]["id"]
+    print("radio_pilih_prompt", radio_pilih_prompt, flush=True)        
+    print("user_input", user_input, flush=True)        
+    print("user_id", user_id, flush=True)        
+    reply = chatGPT(f"{radio_pilih_prompt} {user_input}", user_id)
     say(f"{reply}")
-    say(blocks=input_blocks, text="")
+    # say(blocks=input_blocks, text="")
 
 
 # -----------------------------------
@@ -131,8 +179,8 @@ def trainWithGPT(ack, say, command):
 def train_input(ack, body, say):
     ack()
     print(body)
-    say('Ditunggu ya data sedang dipelajari ⌛')
-    user_id = body["user"]['id']
+    say("Ditunggu ya data sedang dipelajari ⌛")
+    user_id = body["user"]["id"]
     sliced_url = "/".join(body["actions"][0]["value"].split("/")[:4])
     # print(sheet_id)
     if sliced_url == "https://docs.google.com/spreadsheets":
@@ -174,62 +222,94 @@ def train_input(ack, body, say):
 @app.event("app_mention")
 def file_share(body, say, ack):
     ack()
-    say(text='Ditunggu ya data sedang dipelajari ⌛')
+    text_from_mention = body["event"]["blocks"][0]["elements"][0]["elements"][1]['text']
+    user_id = body["event"]["blocks"][0]["elements"][0]["elements"][0]['user_id']
+    try:
+        file_url = body["event"]["files"][0]["url_private_download"]
+    except (KeyError, IndexError):
+        file_url = None
+        
+    # file_url = body["event"]["files"][0]["url_private_download"] | None
     headers = {
         "Authorization": f"Bearer {bot_token}"
     }
-    text_from_mention = body["event"]["blocks"][0]["elements"][0]["elements"][1]['text']
-    user_id = body["event"]["files"][0]["user"]
-    file_url = body["event"]["files"][0]["url_private_download"]
-    filename = os.path.basename(file_url)
-    file_extension = os.path.splitext(filename)[1]
-    delete_user_reply(user_id)
-    r = requests.get(file_url, stream=True, headers=headers)
-    
-    if file_extension == ".pdf":
-        with open(f"train_data_{user_id}{file_extension}", "wb") as file:
-            for chunk in r.iter_content():
-                if chunk:
-                    file.write(chunk)
-        doc = fitz.open(f"train_data_{user_id}{file_extension}")
-        text = ""
-        for page in doc:
-            text += page.get_text()
-        print(text)
-        reply = chatGPT(f"{text_from_mention} {text}", user_id)
-        print("reply", reply)
-        say(f"Data sudah dipelajari: {reply}")
-        say(blocks=input_blocks, text="")
-    elif file_extension == ".csv":
-        with open(f"train_data_{user_id}{file_extension}", "wb") as file:
-            file.write(r.content)
-        # rawdata = open(f"train_data_{user_id}{file_extension}", "rb").read()
-        # encoding = chardet.detect(rawdata)['encoding']
-        # print(encoding)
-        df = pd.read_csv(
-            f"train_data_{user_id}{file_extension}",
-            header=None,
-            index_col=False,
-        )
-        # print(df.to_string())
-        reply = chatGPT(f"{text_from_mention} {df.to_string()}", user_id)
-        print("reply", reply)
-        say(f"Data sudah dipelajari: {reply}")
-        say(blocks=input_blocks, text="")
+    if file_url != None:
+        r = requests.get(file_url, stream=True, headers=headers)
+        # user_id = body["event"]["files"][0]["user"]
+        # file_url = body["event"]["files"][0]["url_private_download"]
+        # filename = os.path.basename(file_url)
+        # file_extension = os.path.splitext(filename)[1]
+        # # delete_user_reply(user_id)
+        # documents = SimpleDirectoryReader('./docs').load_data()
+        # index = GPTSimpleVectorIndex(documents)
+        # response = index.query("")
+        # say(response)
+        # if file_extension == ".pdf":
+        #     say(text='Tunggu sebentar...')
+        #     with open(f"train_data_{user_id}{file_extension}", "wb") as file:
+        #         for chunk in r.iter_content():
+        #             if chunk:
+        #                 file.write(chunk)
+        #     doc = fitz.open(f"train_data_{user_id}{file_extension}")
+        #     text = ""
+        #     for page in doc:
+        #         text += page.get_text()
+        #     print(text)
+        #     reply = chatGPT(f"{text_from_mention} {text}", user_id)
+        #     print("reply", reply)
+        #     say(f"Data sudah dipelajari: {reply}")
+        # elif file_extension == ".csv":
+        #     say(text='Tunggu sebentar...')
+        #     with open(f"train_data_{user_id}{file_extension}", "wb") as file:
+        #         file.write(r.content)
+        #     # rawdata = open(f"train_data_{user_id}{file_extension}", "rb").read()
+        #     # encoding = chardet.detect(rawdata)['encoding']
+        #     # print(encoding)
+        #     df = pd.read_csv(
+        #         f"train_data_{user_id}{file_extension}",
+        #         header=None,
+        #         index_col=False,
+        #     )
+        #     # print(df.to_string())
+        #     reply = chatGPT(f"{text_from_mention} {df.to_string()}", user_id)
+        #     print("reply", reply)
+        #     say(f"Data sudah dipelajari: {reply}")
 
-    elif file_extension == ".xlsx":
-        with open(f"train_data_{user_id}{file_extension}", "wb") as file:
-            file.write(r.content)
-        df = pd.read_excel(
-            f"train_data_{user_id}{file_extension}",
-            header=None,
-            index_col=False,
-        )
-        # print(df.to_string())
-        reply = chatGPT(f"{text_from_mention} {df.to_string()}", user_id)
-        print("reply", reply)
-        say(f"Data sudah dipelajari: {reply}")
-        say(blocks=input_blocks, text="")
+        # elif file_extension == ".xlsx":
+        #     say(text='Tunggu sebentar...')
+        #     say(text='Ditunggu ya data sedang dipelajari ⌛')
+        #     with open(f"train_data_{user_id}{file_extension}", "wb") as file:
+        #         file.write(r.content)
+        #     df = pd.read_excel(
+        #         f"train_data_{user_id}{file_extension}",
+        #         header=None,
+        #         index_col=False,
+        #     )
+        #     # print(df.to_string())
+        #     reply = chatGPT(f"{text_from_mention} {df.to_string()}", user_id)
+        #     print("reply", reply)
+        #     say(f"Data sudah dipelajari: {reply}")
+    else:
+        # With LangChain
+        loader = DirectoryLoader('.docs', glob="**/*.md", loader_cls=CSVLoader)
+        docs = loader.load()
+        say("Tunggu sebentar...")
+        say(f"{docs}")
+
+
+        # With LLamaIndex
+        # documents = SimpleDirectoryReader('./docs').load_data()
+        # index = GPTVectorStoreIndex.from_documents(documents, chunk_size=1000)
+        # query_engine = index.as_query_engine()
+        # response = query_engine.query(f"{text_from_mention}")
+        # say("Tunggu sebentar...")
+        # say(response)
+
+        # Direct training with ChatGPT
+        # reply = chatGPT(f"{text_from_mention}", user_id)
+        # say("Tunggu sebentar...")
+        # say(f"{reply}")
+        
 
 
 from flask import Flask, request
